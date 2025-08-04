a5d10b9994f52979762ed970137d0c4c0bbc5088
maxwell@buffer.finance <maxwell@buffer.finance>
2025-08-04 21:16:01 +0530

    WIP

--- models/docker.py
+++ models/docker.py
@@ -3,5 +3,5 @@ from pydantic import BaseModel, Field
 
 class DockerImage(BaseModel):
     image_name: str = Field(
-        description="Docker image name with optional tag (e.g., 'supurr/algobot:arm')"
+        description="Docker image name with optional tag (e.g., 'supurr/aglotrade:arm')"
     )
