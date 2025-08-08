1b15ca41ce66d0ebe7dc7949bb8dba64a8d993bd
maxwell@buffer.finance <maxwell@buffer.finance>
2025-08-08 17:32:17 +0530

    WIP

--- setup.sh
+++ setup.sh
@@ -150,7 +150,7 @@ echo -e "${GREEN}🐳 Starting required Docker containers and pulling Hummingbot
 
 # Run docker operations in parallel
 docker compose up emqx postgres -d &
-docker pull supurr/algotrade:arm &
+docker pull supurr/algotrade:0.1 &
 
 # Wait for both operations to complete
 wait
