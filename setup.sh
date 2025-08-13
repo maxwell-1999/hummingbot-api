5b6fb9a3e9e18fb92301fe4abb52d49128afcd72
maxwell@buffer.finance <maxwell@buffer.finance>
2025-08-13 17:58:52 +0530

    fix/algo-trade version cahnge

--- setup.sh
+++ setup.sh
@@ -150,7 +150,7 @@ echo -e "${GREEN}🐳 Starting required Docker containers and pulling Hummingbot
 
 # Run docker operations in parallel
 docker compose up emqx postgres -d &
-docker pull supurr/algotrade:0.2 &
+docker pull supurr/algotrade:0.3 &
 
 # Wait for both operations to complete
 wait
